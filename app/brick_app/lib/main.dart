9430de015bb09781ea3ea6fc1e222ac3c34787ee
Kim Shin <kimshin@Kimui-MacBookAir-2.local>
2023-06-30 23:28:51 +0900

    release

---
+++ app/brick_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'HomeScreen.dart';
+import 'binding/binding.dart';
+
+void main() async {
+  // Initialize the camera
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Brick Height Measurement',
+      theme: ThemeData.dark(),
+      home: CameraScreen(),
+      initialBinding: InitBinding(),
+    );
+  }
+}
